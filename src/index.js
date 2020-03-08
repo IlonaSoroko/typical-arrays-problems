cbd3329c1a2ce922dd6e760e56691b70579ec624
IlonaSoroko <ilona.rumak7@gmail.com>
2020-03-09 00:06:09 +0300

    feat: add solution of Typical Arrays Problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!Array.isArray(array) || array.length == 0) {
+    return 0;
+  }
+  return Math.min.apply(0, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!Array.isArray(array) || array.length == 0) {
+    return 0;
+  }
+  return Math.max.apply(0, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!Array.isArray(array) || array.length == 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum / array.length;
 }
